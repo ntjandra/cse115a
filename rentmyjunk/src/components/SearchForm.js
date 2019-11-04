5f1957ff6cdb42ac0d104b91760d0ff8faea61b8
Lilian Gallon <swellchaser33@gmail.com>
2019-11-03 16:38:29 -0800

    implemented search API requests

--- rentmyjunk/src/components/SearchForm.js
+++ rentmyjunk/src/components/SearchForm.js
@@ -12,10 +12,53 @@ class SearchForm extends Form {
 
   handleSubmit(event) {
     const data = new FormData(event.target);
+
+    // First request
+    let contentResult = "";
     const content = data.get("search");
     this.route = "/api/search/item/" + content;
+    var xhr = new XMLHttpRequest();
+    // Listeners
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if(xhr.status  === 200) {
+                // success
+                contentResult = xhr.responseText;
+            } else {
+                // fail
+                contentResult = xhr.statusText;
+            }
+        }
+    };
+    xhr.open(this.method, this.baseUrl + this.route, true);
+    xhr.send(data);
 
-    super.handleSubmit(event);
+    // Second request
+    let placeResult = "";
+    const place = data.get("place");
+    if(place !== "") {
+      this.route = "/api/search/place/" + place;
+      xhr = new XMLHttpRequest();
+      // Listeners
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if(xhr.status  === 200) {
+                // success
+                placeResult = xhr.responseText;
+            } else {
+                // fail
+                placeResult = xhr.statusText;
+            }
+        }
+      };
+      xhr.open(this.method, this.baseUrl + this.route, true);
+      xhr.send(data);
+    }
+
+    // TODO: do something with the results
+    // I can't do it because I need the DB to be populated
+    console.log(contentResult);
+    console.log(placeResult);
   }
 
   render() {
@@ -24,6 +67,7 @@ class SearchForm extends Form {
             <h2>Search</h2>
             <form onSubmit={this.handleSubmit}>
               <input type="text" name="search" placeholder="Search..." required />
+              <input type="text" name="place" placeholder="Place..." />
               <input type="submit" value="Search" />
             </form>
           </div>
