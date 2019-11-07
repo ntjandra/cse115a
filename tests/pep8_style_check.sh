75f053622988cf7472e6d8647828fc425e2e57d7
Lilian Gallon <swellchaser33@gmail.com>
2019-11-06 16:55:55 -0800

    fixed invalid syntax

--- tests/pep8_style_check.sh
+++ tests/pep8_style_check.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-readonly THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 trap exit SIGINT
 
-if ! python -m pycodestyle "${THIS_DIR}/../rmj_oss/populate.py" ; then
+if ! python -m pycodestyle "${DIR}/../rmj_oss/populate.py" ; then
     echo "At least one style issue found"
     exit 1
 fi
 
-if ! python -m pycodestyle "${THIS_DIR}/../rmj_oss/server.py" ; then
+if ! python -m pycodestyle "${DIR}/../rmj_oss/server.py" ; then
     echo "At least one style issue found"
     exit 1
 fi
 
-if ! python -m pycodestyle "${THIS_DIR}/../rmj_oss/database_setup.py" ; then
+if ! python -m pycodestyle "${DIR}/../rmj_oss/database_setup.py" ; then
     echo "At least one style issue found"
     exit 1
 fi
