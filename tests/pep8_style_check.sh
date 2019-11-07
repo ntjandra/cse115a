4fb9cba0c08b99875e544f3538dd4885a4cb983b
Lilian Gallon <swellchaser33@gmail.com>
2019-11-06 17:02:47 -0800

    improved style script to show all issues

--- tests/pep8_style_check.sh
+++ tests/pep8_style_check.sh
@@ -4,21 +4,19 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 trap exit SIGINT
 
+VAL=0
+
 if ! python -m pycodestyle "${DIR}/../rmj_oss/populate.py" ; then
-    echo "At least one style issue found"
-    exit 1
+    VAL=1
 fi
 
 if ! python -m pycodestyle "${DIR}/../rmj_oss/server.py" ; then
-    echo "At least one style issue found"
-    exit 1
+    VAL=1
 fi
 
 if ! python -m pycodestyle "${DIR}/../rmj_oss/database_setup.py" ; then
-    echo "At least one style issue found"
-    exit 1
+    VAL=1
 fi
 
-echo "No issues found"
-exit 0
+exit ${VAL}
 
